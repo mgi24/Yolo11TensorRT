03eef76bbb44d04157bec73560db6d4e48fe409c
mgi24 <ahmadmishbah@student.uns.ac.id>
2025-01-07 09:45:56 +0700

    Signed-off-by: mgi24 <ahmadmishbah@student.uns.ac.id>

--- yolotensor.py
+++ yolotensor.py
@@ -1,5 +1,5 @@
 #USING MODEL IS THE SAME AS NORMAL YOLO
 from ultralytics import YOLO
 
-model = YOLO("RTX2060/yolo11l.engine", task="detect")
+model = YOLO("AllGPUs/yolo11l.engine", task="detect")
 result = model.predict("man.jpg", save=True)
